dd42047f154ac1ff6694c5887c44b4cdc2605f6d
MYSitthichat <bbgan.com@gmail.com>
2024-09-06 22:41:07 +0700

    frist push

---
+++ PRO/login.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'login_page.ui'
+##
+## Created by: Qt User Interface Compiler version 6.7.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QFrame, QLabel, QLineEdit,
+    QMainWindow, QPushButton, QSizePolicy, QStatusBar,
+    QWidget)
+
+class Ui_login(object):
+    def setupUi(self, login):
+        if not login.objectName():
+            login.setObjectName(u"login")
+        login.resize(1320, 900)
+        login.setMinimumSize(QSize(1320, 900))
+        login.setMaximumSize(QSize(1320, 900))
+        login.setStyleSheet(u"background:rgb(255, 238, 172)")
+        self.login_main = QWidget(login)
+        self.login_main.setObjectName(u"login_main")
+        self.login_frame = QFrame(self.login_main)
+        self.login_frame.setObjectName(u"login_frame")
+        self.login_frame.setGeometry(QRect(410, 300, 500, 300))
+        self.login_frame.setMinimumSize(QSize(500, 300))
+        self.login_frame.setMaximumSize(QSize(500, 300))
+        self.login_frame.setStyleSheet(u"background:rgb(167, 224, 255)")
+        self.login_frame.setFrameShape(QFrame.Shape.Box)
+        self.login_frame.setFrameShadow(QFrame.Shadow.Raised)
+        self.login_frame.setLineWidth(3)
+        self.user_label = QLabel(self.login_frame)
+        self.user_label.setObjectName(u"user_label")
+        self.user_label.setGeometry(QRect(220, 15, 61, 51))
+        font = QFont()
+        font.setFamilies([u"TH Niramit AS"])
+        font.setPointSize(24)
+        font.setBold(True)
+        self.user_label.setFont(font)
+        self.user_lineEdit = QLineEdit(self.login_frame)
+        self.user_lineEdit.setObjectName(u"user_lineEdit")
+        self.user_lineEdit.setGeometry(QRect(90, 65, 321, 41))
+        font1 = QFont()
+        font1.setFamilies([u"TH Niramit AS"])
+        font1.setPointSize(20)
+        font1.setBold(True)
+        self.user_lineEdit.setFont(font1)
+        self.user_lineEdit.setStyleSheet(u"background:rgb(255, 255, 255)")
+        self.user_lineEdit.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.password_label = QLabel(self.login_frame)
+        self.password_label.setObjectName(u"password_label")
+        self.password_label.setGeometry(QRect(190, 110, 121, 51))
+        self.password_label.setFont(font)
+        self.passwrod_lineEdit = QLineEdit(self.login_frame)
+        self.passwrod_lineEdit.setObjectName(u"passwrod_lineEdit")
+        self.passwrod_lineEdit.setGeometry(QRect(90, 165, 321, 41))
+        self.passwrod_lineEdit.setFont(font1)
+        self.passwrod_lineEdit.setStyleSheet(u"background:rgb(255, 255, 255)")
+        self.passwrod_lineEdit.setEchoMode(QLineEdit.EchoMode.Password)
+        self.passwrod_lineEdit.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.pushButton = QPushButton(self.login_frame)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(160, 230, 181, 51))
+        self.pushButton.setFont(font)
+        self.pushButton.setStyleSheet(u"background:rgb(72, 216, 0)")
+        login.setCentralWidget(self.login_main)
+        self.statusbar = QStatusBar(login)
+        self.statusbar.setObjectName(u"statusbar")
+        login.setStatusBar(self.statusbar)
+
+        self.retranslateUi(login)
+
+        QMetaObject.connectSlotsByName(login)
+    # setupUi
+
+    def retranslateUi(self, login):
+        login.setWindowTitle(QCoreApplication.translate("login", u"LOGIN", None))
+        self.user_label.setText(QCoreApplication.translate("login", u"USER", None))
+        self.password_label.setText(QCoreApplication.translate("login", u"PASSWORD", None))
+        self.passwrod_lineEdit.setInputMask("")
+        self.pushButton.setText(QCoreApplication.translate("login", u"LOGIN", None))
+    # retranslateUi
+
+if __name__ == "__main__":
+    app = QApplication([])
+    login = QMainWindow()
+    ui = Ui_login()
+    ui.setupUi(login)
+    login.show()
+    app.exec()
---
+++ PRO/main_page.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'main_page.ui'
+##
+## Created by: Qt User Interface Compiler version 6.7.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QComboBox, QFrame, QLabel,
+    QLineEdit, QMainWindow, QPushButton, QSizePolicy,
+    QStatusBar, QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(1320, 900)
+        MainWindow.setMinimumSize(QSize(1320, 900))
+        MainWindow.setMaximumSize(QSize(1320, 900))
+        font = QFont()
+        font.setFamilies([u"TH Niramit AS"])
+        font.setPointSize(20)
+        font.setBold(True)
+        MainWindow.setFont(font)
+        MainWindow.setStyleSheet(u"background:rgb(255, 238, 172)")
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.data_output_frame = QFrame(self.centralwidget)
+        self.data_output_frame.setObjectName(u"data_output_frame")
+        self.data_output_frame.setGeometry(QRect(10, 89, 1120, 781))
+        self.data_output_frame.setStyleSheet(u"background:rgb(255, 255, 255)")
+        self.data_output_frame.setFrameShape(QFrame.Shape.Box)
+        self.data_output_frame.setFrameShadow(QFrame.Shadow.Plain)
+        self.data_output_frame.setLineWidth(3)
+        self.comport_label = QLabel(self.centralwidget)
+        self.comport_label.setObjectName(u"comport_label")
+        self.comport_label.setGeometry(QRect(20, 20, 101, 41))
+        font1 = QFont()
+        font1.setFamilies([u"TH Niramit AS"])
+        font1.setPointSize(22)
+        font1.setBold(True)
+        self.comport_label.setFont(font1)
+        self.comport_comboBox = QComboBox(self.centralwidget)
+        self.comport_comboBox.addItem("")
+        self.comport_comboBox.setObjectName(u"comport_comboBox")
+        self.comport_comboBox.setGeometry(QRect(140, 20, 191, 41))
+        font2 = QFont()
+        font2.setFamilies([u"TH Niramit AS"])
+        font2.setPointSize(24)
+        font2.setBold(True)
+        self.comport_comboBox.setFont(font2)
+        self.comport_comboBox.setStyleSheet(u"background:rgb(255, 255, 255)")
+        self.connect_pushButton = QPushButton(self.centralwidget)
+        self.connect_pushButton.setObjectName(u"connect_pushButton")
+        self.connect_pushButton.setGeometry(QRect(350, 15, 191, 51))
+        self.connect_pushButton.setFont(font2)
+        self.connect_pushButton.setStyleSheet(u"background:rgb(177, 255, 125)")
+        self.weight_label = QLabel(self.centralwidget)
+        self.weight_label.setObjectName(u"weight_label")
+        self.weight_label.setGeometry(QRect(790, 20, 141, 41))
+        self.weight_label.setFont(font1)
+        self.weight_lineEdit = QLineEdit(self.centralwidget)
+        self.weight_lineEdit.setObjectName(u"weight_lineEdit")
+        self.weight_lineEdit.setGeometry(QRect(950, 20, 131, 41))
+        self.weight_lineEdit.setFont(font2)
+        self.weight_lineEdit.setStyleSheet(u"background:rgb(255, 255, 255)")
+        self.weight_lineEdit.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.weight_unit_label = QLabel(self.centralwidget)
+        self.weight_unit_label.setObjectName(u"weight_unit_label")
+        self.weight_unit_label.setGeometry(QRect(1100, 20, 41, 41))
+        self.weight_unit_label.setFont(font1)
+        self.set_zero_pushButton = QPushButton(self.centralwidget)
+        self.set_zero_pushButton.setObjectName(u"set_zero_pushButton")
+        self.set_zero_pushButton.setGeometry(QRect(1150, 15, 161, 51))
+        self.set_zero_pushButton.setFont(font2)
+        self.set_zero_pushButton.setStyleSheet(u"background:rgb(177, 255, 125)")
+        self.start_pushButton = QPushButton(self.centralwidget)
+        self.start_pushButton.setObjectName(u"start_pushButton")
+        self.start_pushButton.setGeometry(QRect(1150, 90, 161, 51))
+        self.start_pushButton.setFont(font2)
+        self.start_pushButton.setStyleSheet(u"background:rgb(73, 213, 34)")
+        self.stop_pushButton = QPushButton(self.centralwidget)
+        self.stop_pushButton.setObjectName(u"stop_pushButton")
+        self.stop_pushButton.setGeometry(QRect(1150, 180, 161, 51))
+        self.stop_pushButton.setFont(font2)
+        self.stop_pushButton.setStyleSheet(u"background:rgb(255, 63, 29)")
+        self.save_pushButton = QPushButton(self.centralwidget)
+        self.save_pushButton.setObjectName(u"save_pushButton")
+        self.save_pushButton.setGeometry(QRect(1150, 270, 161, 51))
+        self.save_pushButton.setFont(font2)
+        self.save_pushButton.setStyleSheet(u"background:rgb(170, 255, 255)")
+        self.logout_pushButton = QPushButton(self.centralwidget)
+        self.logout_pushButton.setObjectName(u"logout_pushButton")
+        self.logout_pushButton.setGeometry(QRect(1150, 820, 161, 51))
+        self.logout_pushButton.setFont(font2)
+        self.logout_pushButton.setStyleSheet(u"background:rgb(255, 255, 255)")
+        self.disconnect_pushButton = QPushButton(self.centralwidget)
+        self.disconnect_pushButton.setObjectName(u"disconnect_pushButton")
+        self.disconnect_pushButton.setGeometry(QRect(560, 15, 191, 51))
+        self.disconnect_pushButton.setFont(font2)
+        self.disconnect_pushButton.setStyleSheet(u"background:rgb(224, 255, 240)")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.comport_label.setText(QCoreApplication.translate("MainWindow", u"COMPORT", None))
+        self.comport_comboBox.setItemText(0, QCoreApplication.translate("MainWindow", u"NONE", None))
+
+        self.connect_pushButton.setText(QCoreApplication.translate("MainWindow", u"CONNECT", None))
+        self.weight_label.setText(QCoreApplication.translate("MainWindow", u"\u0e19\u0e49\u0e33\u0e2b\u0e19\u0e31\u0e01\u0e1b\u0e31\u0e08\u0e08\u0e38\u0e1a\u0e31\u0e19", None))
+        self.weight_unit_label.setText(QCoreApplication.translate("MainWindow", u"KG", None))
+        self.set_zero_pushButton.setText(QCoreApplication.translate("MainWindow", u"SET ZERO", None))
+        self.start_pushButton.setText(QCoreApplication.translate("MainWindow", u"START", None))
+        self.stop_pushButton.setText(QCoreApplication.translate("MainWindow", u"STOP", None))
+        self.save_pushButton.setText(QCoreApplication.translate("MainWindow", u"SAVE", None))
+        self.logout_pushButton.setText(QCoreApplication.translate("MainWindow", u"LOG OUT", None))
+        self.disconnect_pushButton.setText(QCoreApplication.translate("MainWindow", u"DISCONNECT", None))
+    # retranslateUi
+
+
+if __name__ == "__main__":
+    app = QApplication([])
+    login = QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(login)
+    login.show()
+    app.exec()
